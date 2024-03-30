50efa471d63ab2323b5de61465b095310d6298b1
Akarsh Simha <akarshsimha@gmail.com>
2024-03-29 20:44:15 -0700

    Some improvements

--- solar_eclipse.py
+++ solar_eclipse.py
@@ -6,19 +6,21 @@ import subprocess
 import time
 import tqdm
 import sys
+import math
 from enum import Enum
 
 DATE = (2024, 3, 30) # FIXME FIXME FIXME: Change to eclipse date otherwise nothing will happen # FIXME: Expects eclipse to happen on a single UTC date
 
 DEFINE_TIMINGS_UTC = [
-    (0, 59, 0), # First contact (H, M, S)
-    (1, 2, 0), # Second contact (H, M, S)
-    (1, 6, 0), # Third contact (H, M, S)
-    (1, 8, 0), # Fourth contact (H, M, S)
+    (1, 47, 0), # First contact (H, M, S)
+    (1, 52, 0), # Second contact (H, M, S)
+    (1, 55, 0), # Third contact (H, M, S)
+    (2, 1, 0), # Fourth contact (H, M, S)
 ]
 
 DIAMOND_RING = 25  # Go into diamond ring mode at C2/C3 ± these many seconds
 BAILEYS_BEADS = 10 # Go into Bailey's beads mode at C2/C3 ± these many seconds
+TARGET_DIR='Eclipse'
 
 class Config:
     Bracketing ='/main/capturesettings/aeb'
@@ -37,119 +39,6 @@ class Bracketing(Enum):
     EV_1_2_3 = 5
     EV_2 = 6
 
-# class EV(Enum):
-#     # gphoto2 --get-config /main/capturesettings/exposurecompensation
-#     EV_m2   = 0 # -2.0
-#     EV_m1p6 = 1 # -1.6
-#     EV_m1p3 = 2
-#     EV_m1   = 3
-#     EV_m0p6 = 4
-#     EV_m0p3 = 5
-#     EV_0    = 6
-#     EV_p0p3 = 7
-#     EV_p0p6 = 8
-#     EV_p0p9 = 1
-#     EV_p1p3 = 10
-#     EV_p1p6 = 11
-#     EV_p2   = 12
-
-# class ISO(Enum):
-#     _100 = 1
-#     _125 = 2
-#     _160 = 3
-#     _200 = 4
-#     _250 = 5
-#     _320 = 6
-#     _400 = 7
-#     _500 = 8
-#     _640 = 9
-#     _800 = 10
-#     _1000 = 11
-#     _1250 = 12
-#     _1600 = 13
-#     _2000 = 14
-#     _2500 = 15
-#     _3200 = 16
-
-# class Aperture(Enum):
-#     f4_5 = 1
-#     f5 = 2
-#     f5_6 = 3
-#     f6_3 = 4
-#     f7_1 = 5
-#     f8 = 6
-#     f9 = 7
-#     f10 = 8
-#     f11 = 9
-#     f13 = 10
-#     f14 = 11
-#     f16 = 12
-#     f18 = 13
-#     f20 = 14
-#     f22 = 15
-#     f25 = 16
-#     f29 = 17
-#     f32 = 18
-
-# class ShutterSpeed(Enum):
-#     t30 = 1
-#     t25 = 2
-#     t20 = 3
-#     t15 = 4
-#     t13 = 5
-#     t10_3 = 6
-#     t8 = 7
-#     t6_3 = 8
-#     t5 = 9
-#     t4 = 10
-#     t3_2 = 11
-#     t2_5 = 12
-#     t2 = 13
-#     t1_6 = 14
-#     t1_3 = 15
-#     t1 = 16
-#     t0_8 = 17
-#     t0_6 = 18
-#     t0_5 = 19
-#     t0_4 = 20
-#     t0_3 = 21
-#     s4 = 22
-#     s5 = 23
-#     s6 = 24
-#     s8 = 25
-#     s10 = 26
-#     s13 = 27
-#     s15 = 28
-#     s20 = 29
-#     s25 = 30
-#     s30 = 31
-#     s40 = 32
-#     s50 = 33
-#     s60 = 34
-#     s80 = 35
-#     s100 = 36
-#     s125 = 37
-#     s160 = 38
-#     s200 = 39
-#     s250 = 40
-#     s320 = 41
-#     s400 = 42
-#     s500 = 43
-#     s640 = 44
-#     s800 = 45
-#     s1000 = 46
-#     s1250 = 47
-#     s1600 = 48
-#     s2000 = 49
-#     s2500 = 50
-#     s3200 = 51
-#     s4000 = 52
-#     s5000 = 53
-#     s6400 = 54
-#     s8000 = 55
-
-
-
 class Settings:
     interval = 0
 
@@ -163,39 +52,46 @@ class Settings:
 
 class Phases:
     class Partial(Settings):
+        name='Partial'
         interval = 120 # Take a photo every 2 minutes
         aperture = "10" # f/10 (filtered)
         bracketing = Bracketing.EV_1 # ±1
         speed = "1/1000"
 
     class Diamond(Settings):
+        name='DiamondRing'
         aperture = "11" # f/11
         bracketing = Bracketing.EV_1_1_3 # ±1⅓
         speed = "1/4000"
 
     class Baileys(Settings):
+        name='Baileys'
         aperture = "16"
         bracketing = Bracketing.EV_1_2_3 # ±1⅔
         speed = "1/3200"
 
     class Totality(Settings):
+        name='Totality'
         bracketing = Bracketing.OFF
         interval = 5
         delay_compensation = 3
         exposures = [
-            ("16", "1/1600", 200), # Chromosphere
-            ("11", "1/1600", 200), # Prominences
-            ("10", "1/500", 200),  # Lower Corona
-            ("10", "1/60", 200),   # Inner Corona
-            ("8",  "1/25", 400),   # Middle Corona
-            ("6.3", "1/10", 400),  # Outer Corona
-            ("6.3", "0.5", 400), # Far Outer Corona
-            ("6.3", "1", 400),   # Earthshine
+            ("16", "1/1600", 200), # Chromosphere    [EV: 18.6]
+            ("11", "1/1600", 200), # Prominences     [EV: 17.6]
+            ("10", "1/500", 200),  # Lower Corona    [EV: 15.6]
+            ("10", "1/60", 200),   # Inner Corona    [EV: 12.5]
+            ("8",  "1/25", 400),   # Middle Corona   [EV: 9.6]
+            ("6.3", "1/10", 400),  # Outer Corona    [EV: 7.6]
+            ("6.3", "0.5", 400), # Far Outer Corona  [EV: 5.3]
+            ("6.3", "1", 400),   # Earthshine        [EV: 4.3]
+            ("6.3", "4", 200),   # Earthshine        [EV: 2.3]
         ]
 
 
-def click(aperture: str, speed: str, iso: int, bracketing: Bracketing):
-    cmd = f"gphoto2 --set-config-value {Config.Aperture}={aperture} --set-config-value {Config.ShutterSpeed}={speed} --set-config-value {Config.ISO}={iso} --set-config {Config.Bracketing}={bracketing.value} --set-config capturetarget=0 --force-overwrite --filename='Eclipse/t{int(time.time())}_%n' --no-keep --capture-image-and-download"
+def click(aperture: str, speed: str, iso: int, phase: Phases):
+    filename = os.path.join(TARGET_DIR, f'{phase.name}_t{int(time.time())}_%n')
+    bracketing = phase.bracketing
+    cmd = f"gphoto2 --set-config-value {Config.Aperture}={aperture} --set-config-value {Config.ShutterSpeed}={speed} --set-config-value {Config.ISO}={iso} --set-config {Config.Bracketing}={bracketing.value} --set-config capturetarget=0 --force-overwrite --filename='{filename}' --no-keep --capture-image-and-download"
     if bracketing != Bracketing.OFF:
         cmd += " --capture-image-and-download --capture-image-and-download"
     print(cmd)
@@ -216,11 +112,11 @@ def main():
         say.festival_proc.stdin.write(f'(SayText "{text}")\n'.encode())
         say.festival_proc.stdin.flush()
 
+    if not os.path.isdir(f'{TARGET_DIR}'):
+        os.makedirs(f'{TARGET_DIR}')
+
     phase = None
     TIMES = [datetime.datetime(*DATE, *time, 0, datetime.timezone.utc) for time in DEFINE_TIMINGS_UTC]
-
-    if not os.path.isdir('Eclipse/'):
-        os.makedirs('Eclipse/')
 
     if TIMES[0].date() != datetime.datetime.today().date():
         print(f'!!!! Warning: Eclipse does not seem to be today, i.e. {datetime.datetime.today().date()} !!!!')
@@ -319,7 +215,7 @@ def main():
                     set_bracketing(phase.bracketing)
                 if int(time.time() % Phases.Partial.interval) == 0:
                     print('Clicking partial phase exposure')
-                    click(phase.aperture, phase.speed, phase.iso, bracketing=phase.bracketing)
+                    click(phase.aperture, phase.speed, phase.iso, phase=phase)
 
                 dt = int((C2_DR - now()).total_seconds())
                 if dt > 0 and dt < 60 and int(dt) % 10 == 0 and int(dt) not in c2dr_countdown:
@@ -332,7 +228,7 @@ def main():
                     say('Camera entering diamond ring phase. Ensure filter is off!')
                     phase = Phases.Diamond
                     set_bracketing(phase.bracketing)
-                click(phase.aperture, phase.speed, phase.iso, bracketing=phase.bracketing)
+                click(phase.aperture, phase.speed, phase.iso, phase=phase)
 
             while (now() > C2_BB and now() <= C2_BB2) or (now() >= C3_BB2 and now() < C3_BB):
                 time.sleep(0.05)
@@ -340,7 +236,7 @@ def main():
                     say('Camera entering Bailey phase. Ensure filter is off!')
                     phase = Phases.Baileys
                     set_bracketing(phase.bracketing)
-                click(phase.aperture, phase.speed, phase.iso, bracketing=phase.bracketing)
+                click(phase.aperture, phase.speed, phase.iso, phase=phase)
 
             while (now() > C2_BB2 and now() < C3_BB2):
                 try:
@@ -355,7 +251,7 @@ def main():
                     phase = Phases.Totality
                     set_bracketing(phase.bracketing)
                 for exposure in phase.exposures:
-                    click(*exposure, bracketing=phase.bracketing)
+                    click(*exposure, phase=phase)
                 dt = (C3_BB2 - now()).total_seconds()
                 if dt < 0:
                     continue
